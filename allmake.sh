186ab1b3242cd484914b7168e8661eaa62b56607
John Williamson <johnhw@gmail.com>
2016-08-23 22:34:30 +0100

    Added missing dependencies

---
+++ allmake.sh
@@ -0,0 +1,9 @@
+rm bootfiles.zip
+make
+make luajit.fmap
+make luajit_fmap.o
+make
+make luajit.fmap
+make luajit_fmap.o
+make
+
---
+++ sqlite_stubs.c
@@ -0,0 +1,12 @@
+int sqlite3_os_init(void)
+{
+    // do nothing for now...
+    return 0;
+}
+
+
+int sqlite3_os_end(void)
+{
+    // do nothing for now...
+    return 0;
+}
