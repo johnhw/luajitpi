2393941c31a7252e84790d46e89854aa5f64b7c5
johnhw <johnhw@users.noreply.github.com>
2016-08-30 21:23:45 +0100

    Update cc.lua
    Fixed bug in cc.lua that ignored the passed C string

--- lua/cc.lua
+++ lua/cc.lua
@@ -6,7 +6,7 @@ void *getsymbol_tcc(void *tcc, const char *str);
 ]])
 
 function cc(fn, proto, c_code)
-    local tcc_context = ffi.C.compile_tcc(deadbeef)
+    local tcc_context = ffi.C.compile_tcc(c_code)
     func = ffi.C.getsymbol_tcc(compiled, fn)
     func = ffi.cast(proto, func)
     return func    
